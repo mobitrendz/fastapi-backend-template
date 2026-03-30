d7801ee0b906309cfed7e3c383d6dfe4dfe7d8cd
mobitrendz <sreeraj.sreenivasan@gmail.com>
2026-03-31 06:55:53 +1000

    Updated Welcome API

--- app/api/welcome.py
+++ app/api/welcome.py
@@ -5,3 +5,7 @@ router = APIRouter(prefix="", tags=["welcome"])
 @router.get("/")
 def get_welcome_message():
     return {"message": "Welcome User!"}
+
+@router.get("/{user_name}")
+def get_welcome_message_user(user_name: str):
+    return {"message": "Welcome " + user_name + "!"}
--- app/main.py
+++ app/main.py
@@ -3,8 +3,4 @@ from app.api import welcome
 
 app = FastAPI()
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World!"}
-
 app.include_router(welcome.router)
